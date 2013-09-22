40634b582416722c65853da8e56051ce9e70fc5f
Andrew Dai <theman@andrewdai.co>
2013-09-21 23:34:12 -0400

    Finished bruteforce solution 	modified:   src/Brute.java

--- src/Brute.java
+++ src/Brute.java
@@ -2,27 +2,43 @@
 public class Brute {
 
 	public static void main(String [] args){
-		int N = StdIn.readInt();
-		Point[] p = new Point[N];
-		for(int i = 0; i < N; i++){
-			p[0] = new Point(StdIn.readInt(), StdIn.readInt());
-		}
-
+		
 		StdDraw.setXscale(0, 32768);
 		StdDraw.setYscale(0, 32768);
+		
+		String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] p = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            p[i] = new Point(x, y);
+            p[i].draw();
+        }
 
 		for(int i = 0; i < N; i++){
-			p[i].draw();
-			for(int j = i; j < N; j++)
-				for(int k = j; k < N; k++)
-					for(int l = k; l < N; l++)
-						if(p[i].slopeTo(p[j]) == p[i].slopeTo(p[k]) 
-						&& p[i].slopeTo(p[j]) == p[i].slopeTo(p[l])){
-							System.out.println(p[i] + " -> " + p[j]  + " -> " + p[k] + " -> " + p[l]);
+			for(int j = i+1; j < N; j++){
+				
+				double ij = p[i].slopeTo(p[j]);
+				for(int k = j+1; k < N; k++){
+				
+					double jk = p[j].slopeTo(p[k]);
+					if(ij != jk) {
+						continue;
+					}
+					for(int l = k+1; l < N; l++){
+					
+						double kl = p[k].slopeTo(p[l]);
+						if(ij == kl){
+							StdOut.println(p[i] + " -> " + p[j]  + " -> " + p[k] + " -> " + p[l]);
 							p[i].drawTo(p[j]);
 							p[i].drawTo(p[k]);
 							p[i].drawTo(p[l]);
 						}
+					}
+				}
+			}
 		}
 	}
 
