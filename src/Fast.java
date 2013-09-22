993a861a81e3d50a7a15ab2ea16c020e94f298ca
Andrew Dai <theman@andrewdai.co>
2013-09-22 15:46:38 -0400

    adding framework... need to implement sort 	modified:   src/Fast.java

--- src/Fast.java
+++ src/Fast.java
@@ -22,14 +22,30 @@ public class Fast {
 		
 		for(int i = 0; i < N; i++){
 			Map m = new Map(N-1);
+			// insert into map
 			boolean skip = false;
 			for(int j = 0; j < N-1; j++){
 				if(j == i) { skip = true; continue; }
 				if(skip) m.m[j-1] = new Node(p[i].slopeTo(p[j]), p[j]);
 			}
-
+			
+			// sort map
+			sort(m);
+			
+			for(int j = 0; j < N-1-2; j++){
+				if(m.m[j].compareTo(m.m[j+1]) == 0)
+					if(m.m[j+1].compareTo(m.m[j+2]) == 0){
+						// match
+						StdOut.println(p[i] + " -> " + m.m[j].p + " -> " + m.m[j+1].p + " -> " + m.m[j+2].p);
+					}
+					else j++;
+			}
 		}
 
+	}
+	
+	private static void sort(Map m){
+		
 	}
 	
 	private static class Map{
@@ -41,13 +57,20 @@ public class Fast {
 		
 	}
 	
-	private static class Node{
+	private static class Node implements Comparable<Node>{
 		public final double key;
 		public final Point p;
 		
 		public Node(double key, Point p){
 			this.key = key;
 			this.p = p;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			if( key < o.key ) return -1;
+			else if ( key == o.key ) return 0;
+			return 1;
 		}
 	}
 
