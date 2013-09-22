6d58228c1947da2fae7cf97d0b9d22892bd94fd8
Andrew Dai <theman@andrewdai.co>
2013-09-22 00:13:45 -0400

    adding fast implementation 	modified:   src/Fast.java

--- src/Fast.java
+++ src/Fast.java
@@ -5,8 +5,49 @@ public class Fast {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		StdDraw.setXscale(0, 32768);
+		StdDraw.setYscale(0, 32768);
+
+		String filename = args[0];
+		In in = new In(filename);
+		int N = in.readInt();
+		Point[] p = new Point[N];
+		for (int i = 0; i < N; i++) {
+			int x = in.readInt();
+			int y = in.readInt();
+			p[i] = new Point(x, y);
+			p[i].draw();
+		}
+		
+		for(int i = 0; i < N; i++){
+			Point[] t = new Point[N-1];
+			for(int j = 0; j < N-1; j++){
+				if(j == i) continue;
+				
+			}
+
+		}
+
+	}
+	
+	private class Map{
+		
+		public Node[] m;
+		public Map(int N){
+			m = new Node[N];
+		}
+		
+	}
+	
+	private class Node{
+		public final double key;
+		public final Point p;
+		
+		public Node(double key, Point p){
+			this.key = key;
+			this.p = p;
+		}
 	}
 
 }
